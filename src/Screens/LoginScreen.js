314d9c7fc15b2a184caf7f2d3b848b89e2c719ce
SonicAi07 <sonicilyas077@gmail.com>
2023-01-15 22:45:28 +0530

    added email alert in account page

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from '../Components/Login/Login'
 import Register from '../Components/Register/Register'
 import LoginImage from '../assets/login.png'
@@ -11,30 +11,43 @@ function LoginScreen() {
     const userData = useSelector(getUser)
     const navigate = useNavigate()
 
+    const [isAlert, setIsAlert] = useState(false)
+
     useEffect(() => {
         if (userData !== null) {
             navigate('/')
         }
+        setIsAlert(true)
     }, [navigate, userData])
 
     return (
-        <div
-            className='flex laptop:flex-row 
+        <>
+            <div
+                className='flex laptop:flex-row 
         laptop:justify-evenly laptop:items-start pt-10
         sm:flex-col-reverse sm:items-center sm:justify-start pb-3
         relative
         '
-        >
-            <Login />
-            <img
-                src={LoginImage}
-                alt="LoginImage"
-                className='w-1/3 hidden laptop:block'
-            />
-            <Register
-            />
+            >
+                <Login />
+                <img
+                    src={LoginImage}
+                    alt="LoginImage"
+                    className='w-1/3 hidden laptop:block'
+                />
+                <Register
+                />
 
-        </div>
+            </div>
+
+            <div className={isAlert ? 'fixed left-2 rounded-lg shadow-lg bottom-8 bg-white transition' : "hidden transition"}>
+                <div className='border w-96 px-4 py-5'>
+                    <p>Please Provide the Valid Email Address</p>
+                    <p><b>One Time Password</b> is needed during Registration..!</p>
+                    <button className='border px-3 py-1 mt-3' onClick={() => setIsAlert(false)}>Close</button>
+                </div>
+            </div>
+        </>
     )
 }
 
